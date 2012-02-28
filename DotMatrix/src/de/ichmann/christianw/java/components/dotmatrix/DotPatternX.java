21d82cf9e317cd64244de92220a8e354cc5c0076
Christian Wichmann <prometheus42@users.noreply.github.com>
2012-02-28 19:08:13 +0000

    * Initial import from file system Date: 2011-07-26 09:42:13

---
+++ DotMatrix/src/de/ichmann/christianw/java/components/dotmatrix/DotPatternX.java
@@ -0,0 +1,49 @@
+package de.ichmann.christianw.java.components.dotmatrix;
+
+/**
+ * Dot pattern for the character 'X'
+ * 
+ * @author Christian Wichmann
+ * @version 0.1
+ */
+public class DotPatternX extends DotPattern {
+
+	DotPatternX() {
+		this.fillPattern();
+	}
+
+	/**
+	 * Method must be overwritten by concrete pattern class to fill the array
+	 */
+	protected void fillPattern() {
+		int[] list = new int[] { 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0,
+				0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0,
+				0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 };
+
+		int n = 0;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				pattern[i][j] = (list[n++] != 0);
+			}
+		}
+	}
+
+	@Override
+	public boolean[][] getPattern() {
+		return pattern;
+	}
+
+	@Override
+	public boolean getDot(int x, int y) {
+		if (x < columns && y < rows)
+			return pattern[y][x];
+		else
+			return false;
+	}
+
+	@Override
+	public String toString() {
+		return "X";
+	}
+
+}
