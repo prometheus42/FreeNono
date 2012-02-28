8dd1e93e621bbcdf813e57a40d689a698157dacc
Christian Wichmann <prometheus42@users.noreply.github.com>
2012-02-28 19:36:41 +0000

    * Added copyright notices to various files * Changed file access mechanism in AudioProvider Date: 2011-07-30 19:37:52

--- FreeNono/src/org/freenono/board/BoardPreview.java
+++ FreeNono/src/org/freenono/board/BoardPreview.java
@@ -1,3 +1,20 @@
+/*****************************************************************************
+ * FreeNono - A free implementation of the nonogram game
+ * Copyright (c) 2010 Markus Wichmann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
 package org.freenono.board;
 
 import java.awt.Dimension;
@@ -16,14 +33,14 @@ import org.freenono.event.GameEventHelper;
 import org.freenono.model.Game;
 import org.freenono.model.Token;
 
-
 /**
  * Builds a preview image of the running game represented by the Game object. At
  * changes on the board, the image is rebuild by calling the refreshPreview()
  * method. This class is Cloneable, so that it can be used in more than one gui
  * component at a time.
  * 
- * (More information on scaling of images: http://today.java.net/pub/a/today/2007
+ * (More information on scaling of images:
+ * http://today.java.net/pub/a/today/2007
  * /04/03/perils-of-image-getscaledinstance.html)
  * 
  */
