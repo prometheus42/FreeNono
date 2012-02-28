210aca3e92953b0d51980d940f0ad14b0981f544
Christian Wichmann <prometheus42@users.noreply.github.com>
2012-02-28 19:07:29 +0000

    * Added log4j link to README * Changed package name to de.ichmann.markusw.java.apps.freenono Date: 2011-07-25 12:42:18

---
+++ FreeNono/src/de/ichmann/markusw/java/apps/freenono/ui/MainUI.java
@@ -0,0 +1,574 @@
+/*****************************************************************************
+ * FreeNono - A free implementation of the nonogram game
+ * Copyright (c) 2010 Markus Wichmann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+package de.ichmann.markusw.java.apps.freenono.ui;
+
+import java.awt.BorderLayout;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
+
+import de.ichmann.markusw.java.apps.freenono.event.GameListener;
+import de.ichmann.markusw.java.apps.freenono.exception.InvalidArgumentException;
+import de.ichmann.markusw.java.apps.freenono.model.Game;
+import de.ichmann.markusw.java.apps.freenono.model.GameState;
+import de.ichmann.markusw.java.apps.freenono.model.Manager;
+import de.ichmann.markusw.java.apps.freenono.model.Nonogram;
+import de.ichmann.markusw.java.apps.freenono.model.RandomNonogram;
+import de.ichmann.markusw.java.apps.freenono.model.Settings;
+import de.ichmann.markusw.java.apps.freenono.model.RandomNonogram.RandomTypes;
+import de.ichmann.markusw.java.apps.freenono.sound.AudioProvider;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeModel;
+
+import java.awt.ComponentOrientation;
+
+import org.apache.log4j.Logger;
+
+public class MainUI extends JFrame implements Observer {
+
+	private static final long serialVersionUID = 3834029197472615118L;
+
+	private static Logger logger = Logger.getLogger(MainUI.class); // @jve:decl-index=0:
+
+	private GameListener gameListener = new GameListener() {
+
+		@Override
+		public void Timer() {
+			boardControl.refreshTime();
+			boardControl.repaint();
+		}
+
+		@Override
+		public void StateChanged(GameState oldState, GameState newState) {
+			boolean isSolved = true;
+
+			switch (newState) {
+			case gameOver:
+				isSolved = false;
+			case solved:
+				stopButton.setEnabled(false);
+				pauseButton.setEnabled(false);
+				getCurrentGame().solveGame();
+				boardControl.refresh();
+				boardControl.repaint();
+				GameOverUI ui = new GameOverUI(getCurrentGame(), boardControl.getPreviewImage(), isSolved);
+				ui.setVisible(true);
+				break;
+			default:
+				break;
+			}
+
+		}
+
+		@Override
+		public void FieldOccupied(int x, int y) {
+		}
+
+		@Override
+		public void FieldMarked(int x, int y) {
+		}
+	};
+
+	private Manager manager = null;
+	private Game currentGame = null;
+	private Nonogram currentNonogram = null;
+	private AudioProvider audioProvider = null;
+
+	private JPanel jContentPane = null;
+	private JToolBar statusBar = null;
+	private JMenuItem statusBarText = null;
+	private BoardControl boardControl = null;
+
+	private JToolBar jJToolBarBar = null;
+
+	private JButton startButton = null;
+
+	private JButton pauseButton = null;
+
+	private JButton resumeButton = null;
+
+	private JButton stopButton = null;
+
+	private JButton restartButton = null;
+
+	private JButton exitButton = null;
+
+	private JButton aboutButton = null;
+
+	private JButton optionsButton = null;
+
+	/**
+	 * This is the default constructor
+	 */
+	public MainUI() {
+		super();
+
+		// show splash screen
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				SplashScreen splash = new SplashScreen();
+				splash.setVisible(true);
+			}
+		});
+
+		// instantiate game manager
+		try {
+			manager = new Manager();
+		} catch (InvalidArgumentException e) {
+			// TODO handle exception correct
+			// TODO add log or user message
+			manager = null;
+		} catch (FileNotFoundException e) {
+			// TODO handle exception correct
+			// TODO add log or user message
+			manager = null;
+		} catch (IOException e) {
+			// TODO handle exception correct
+			// TODO add log or user message
+			manager = null;
+		}
+
+		// instantiate audio provider for game sounds
+		audioProvider = new AudioProvider(manager.getSettings().getPlayAudio());
+
+		// initialize MainUI
+		initialize();
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		this.setSize(950, 780);
+		this.setLocationRelativeTo(null);
+		this.setName("mainUI"); //$NON-NLS-1$
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(getJContentPane());
+		this.setTitle(Messages.getString("MainUI.Title")); //$NON-NLS-1$
+	}
+
+	/**
+	 * This method initializes jContentPane
+	 * 
+	 * @return javax.swing.JPanel
+	 */
+	private JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jContentPane = new JPanel();
+			jContentPane.setLayout(new BorderLayout());
+			jContentPane.add(getBoardControl(), BorderLayout.CENTER);
+			jContentPane.add(getStatusBar(), BorderLayout.SOUTH);
+			jContentPane.add(getJJToolBarBar(), BorderLayout.NORTH);
+		}
+		return jContentPane;
+	}
+
+	/**
+	 * This method initializes statusBar
+	 * 
+	 * @return javax.swing.JToolBar
+	 */
+	private JToolBar getStatusBar() {
+		if (statusBar == null) {
+			statusBar = new JToolBar();
+			statusBar.add(getStatusBarText());
+		}
+		return statusBar;
+	}
+
+	/**
+	 * This method initializes statusBarText
+	 * 
+	 * @return javax.swing.JMenuItem
+	 */
+	private JMenuItem getStatusBarText() {
+		if (statusBarText == null) {
+			statusBarText = new JMenuItem();
+		}
+		return statusBarText;
+	}
+
+	public Game getCurrentGame() {
+		return currentGame;
+	}
+
+	public void setCurrentGame(Game game) {
+
+		if (this.currentGame != null) {
+			this.currentGame.removeGameListener(gameListener);
+		}
+
+		this.currentGame = game;
+
+		if (this.currentGame != null) {
+			currentGame.addGameListener(gameListener);
+			pauseButton.setEnabled(true);
+			stopButton.setEnabled(true);
+			boardControl.startGame(game);
+		} else {
+			pauseButton.setEnabled(false);
+			stopButton.setEnabled(false);
+			boardControl.stopGame();
+		}
+
+		// add audioProvider as EventListener
+		if (this.currentGame != null) {
+			audioProvider.addAsListener(currentGame);
+		}
+	}
+
+	public Nonogram getCurrentNonogram() {
+		return currentNonogram;
+	}
+
+	public void setCurrentNonogram(Nonogram currentNonogram) {
+
+		this.currentNonogram = currentNonogram;
+		restartButton.setEnabled(currentNonogram != null);
+	}
+
+	private void performStart() {
+
+		// create UI and fill tree
+		NonogramChooserUI nonoChooser = new NonogramChooserUI();
+		
+		String[] dirs = manager.getNonogramDirList().toArray(new String[0]);
+		for (int i = 0; i < dirs.length; i++) {
+			Object[] array = manager.getNonogramList(dirs[i]).toArray();
+			nonoChooser.addNonogramsToTree(dirs[i], array);
+		}
+
+		// show UI
+		nonoChooser.setVisible(true);
+
+		// TODO: clean up these ifs and switchs...
+		if (nonoChooser.isValidOptions()) {
+			// evaluate chosen options
+			int type = nonoChooser.getType();
+
+			Nonogram obj = null;
+
+			switch (type) {
+			case 0: // nonogram by file
+				obj = nonoChooser.getChoosenNono();
+				break;
+			case 1: // random nonogram
+				RandomNonogram rnono = new RandomNonogram();
+				obj = rnono.createRandomNonogram(nonoChooser.getSliderHeight(), nonoChooser.getSliderWidth(), nonoChooser.getRandomType());
+				break;
+			case 2: // TODO: nonogram by seed
+
+				break;
+			default:
+				break;
+			}
+
+			// start nonogram
+			setCurrentNonogram(obj);
+			if (obj != null) {
+				setCurrentGame(manager.createGame(getCurrentNonogram()));
+			} else {
+				setCurrentGame(null);
+			}
+		} else {
+			setCurrentNonogram(null);
+			setCurrentGame(null);
+		}
+	}
+
+	private void performRestart() {
+
+		if (getCurrentNonogram() != null) {
+			setCurrentGame(manager.createGame(getCurrentNonogram()));
+		}
+		// TODO check implementation
+	}
+
+	private void performPause() {
+
+		boardControl.pauseGame();
+		pauseButton.setEnabled(false);
+		resumeButton.setEnabled(true);
+		// TODO check implementation
+	}
+
+	private void performResume() {
+
+		boardControl.resumeGame();
+		pauseButton.setEnabled(true);
+		resumeButton.setEnabled(false);
+		// TODO check implementation
+	}
+
+	private void performStop() {
+
+		setCurrentGame(null);
+		setCurrentNonogram(null);
+		// TODO check implementation
+	}
+
+	private void performExit() {
+		// TODO implement additional user question
+		manager.quitProgram();
+		audioProvider.quitProgram();
+		this.setVisible(false);
+		this.dispose();
+	}
+
+	private void showAbout() {
+		AboutUI ui = new AboutUI(this);
+		ui.setVisible(true);
+	}
+
+	private void showOptions() {
+		Settings s = manager.getSettings();
+		OptionsUI ui = new OptionsUI(this, s);
+		ui.setVisible(true);
+		s.toString();
+	}
+
+	/**
+	 * This method initializes boardControl
+	 * 
+	 * @return javax.swing.JPanel
+	 */
+	private BoardControl getBoardControl() {
+		if (boardControl == null) {
+			boardControl = new BoardControl();
+		}
+		return boardControl;
+	}
+
+	/**
+	 * This method initializes jJToolBarBar
+	 * 
+	 * @return javax.swing.JToolBar
+	 */
+	private JToolBar getJJToolBarBar() {
+		if (jJToolBarBar == null) {
+			jJToolBarBar = new JToolBar();
+			jJToolBarBar.setFloatable(false);
+			jJToolBarBar.add(getStartButton());
+			jJToolBarBar.add(getRestartButton());
+			jJToolBarBar.add(getPauseButton());
+			jJToolBarBar.add(getResumeButton());
+			jJToolBarBar.add(getStopButton());
+			jJToolBarBar.add(getOptionsButton());
+			jJToolBarBar.add(getAboutButton());
+			jJToolBarBar.add(getExitButton());
+		}
+		return jJToolBarBar;
+	}
+
+	/**
+	 * This method initializes startButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getStartButton() {
+		if (startButton == null) {
+			startButton = new JButton();
+			startButton.setText(""); //$NON-NLS-1$
+			startButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_start.png"))); //$NON-NLS-1$
+			startButton.setToolTipText(Messages.getString("MainUI.StartTooltip")); //$NON-NLS-1$
+			startButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_start2.png"))); //$NON-NLS-1$
+			startButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					performStart();
+				}
+			});
+		}
+		return startButton;
+	}
+
+	/**
+	 * This method initializes pauseButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getPauseButton() {
+		if (pauseButton == null) {
+			pauseButton = new JButton();
+			pauseButton.setToolTipText(Messages.getString("MainUI.PauseTooltip")); //$NON-NLS-1$
+			pauseButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_pause.png"))); //$NON-NLS-1$
+			pauseButton.setText(""); //$NON-NLS-1$
+			pauseButton.setEnabled(false);
+			pauseButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_pause2.png"))); //$NON-NLS-1$
+			pauseButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					performPause();
+				}
+			});
+		}
+		return pauseButton;
+	}
+
+	/**
+	 * This method initializes resumeButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getResumeButton() {
+		if (resumeButton == null) {
+			resumeButton = new JButton();
+			resumeButton.setToolTipText(Messages.getString("MainUI.ResumeTooltip")); //$NON-NLS-1$
+			resumeButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_resume.png"))); //$NON-NLS-1$
+			resumeButton.setText(""); //$NON-NLS-1$
+			resumeButton.setEnabled(false);
+			resumeButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_resume2.png"))); //$NON-NLS-1$
+			resumeButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					performResume();
+				}
+			});
+		}
+		return resumeButton;
+	}
+
+	/**
+	 * This method initializes stopButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getStopButton() {
+		if (stopButton == null) {
+			stopButton = new JButton();
+			stopButton.setToolTipText(Messages.getString("MainUI.StopTooltip")); //$NON-NLS-1$
+			stopButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_stop.png"))); //$NON-NLS-1$
+			stopButton.setText(""); //$NON-NLS-1$
+			stopButton.setEnabled(false);
+			stopButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_stop2.png"))); //$NON-NLS-1$
+			stopButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					performStop();
+				}
+			});
+		}
+		return stopButton;
+	}
+
+	/**
+	 * This method initializes restartButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getRestartButton() {
+		if (restartButton == null) {
+			restartButton = new JButton();
+			restartButton.setToolTipText(Messages.getString("MainUI.RestartTooltip")); //$NON-NLS-1$
+			restartButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_restart.png"))); //$NON-NLS-1$
+			restartButton.setText(""); //$NON-NLS-1$
+			restartButton.setEnabled(false);
+			restartButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_restart2.png"))); //$NON-NLS-1$
+			restartButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					performRestart();
+				}
+			});
+		}
+		return restartButton;
+	}
+
+	/**
+	 * This method initializes exitButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getExitButton() {
+		if (exitButton == null) {
+			exitButton = new JButton();
+			exitButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_exit.png"))); //$NON-NLS-1$
+			exitButton.setEnabled(true);
+			exitButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_exit2.png"))); //$NON-NLS-1$
+			exitButton.setText(""); //$NON-NLS-1$
+			exitButton.setToolTipText(Messages.getString("MainUI.ExitTooltip")); //$NON-NLS-1$
+			exitButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					performExit();
+				}
+			});
+		}
+		return exitButton;
+	}
+
+	/**
+	 * This method initializes aboutButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getAboutButton() {
+		if (aboutButton == null) {
+			aboutButton = new JButton();
+			aboutButton.setEnabled(true);
+			aboutButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_about2.png"))); //$NON-NLS-1$
+			aboutButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_about.png"))); //$NON-NLS-1$
+			aboutButton.setText(""); //$NON-NLS-1$
+			aboutButton.setComponentOrientation(ComponentOrientation.UNKNOWN);
+			aboutButton.setToolTipText(Messages.getString("MainUI.AboutTooltip")); //$NON-NLS-1$
+			aboutButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					showAbout();
+				}
+			});
+		}
+		return aboutButton;
+	}
+
+	/**
+	 * This method initializes optionsButton
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getOptionsButton() {
+		if (optionsButton == null) {
+			optionsButton = new JButton();
+			optionsButton.setComponentOrientation(ComponentOrientation.UNKNOWN);
+			optionsButton.setToolTipText(Messages.getString("MainUI.OptionsTooltip")); //$NON-NLS-1$
+			optionsButton.setDisabledIcon(new ImageIcon(getClass().getResource("/icon/button_config2.png"))); //$NON-NLS-1$
+			optionsButton.setIcon(new ImageIcon(getClass().getResource("/icon/button_config.png"))); //$NON-NLS-1$
+			optionsButton.setText(""); //$NON-NLS-1$
+			optionsButton.setEnabled(true);
+			optionsButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					showOptions();
+				}
+			});
+		}
+		return optionsButton;
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+} // @jve:decl-index=0:visual-constraint="10,10"
---
+++ FreeNono/src/de/ichmann/markusw/java/apps/freenono/ui/NonogramChooserUI.java
@@ -0,0 +1,334 @@
+/*****************************************************************************
+ * FreeNono - A free implementation of the nonogram game
+ * Copyright (c) 2010 Markus Wichmann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+package de.ichmann.markusw.java.apps.freenono.ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.SliderUI;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTree;
+
+import de.ichmann.markusw.java.apps.freenono.model.Nonogram;
+import de.ichmann.markusw.java.apps.freenono.model.RandomNonogram.RandomTypes;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JSlider;
+
+public class NonogramChooserUI extends JDialog {
+
+	private static final long serialVersionUID = -6359071603972829942L;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtNotYetImplemented;
+	private JComboBox comboBox;
+	private JTree tree;
+	private DefaultTreeModel treeModel;
+	private JRadioButton rdbtnPredefinedNonograms;
+	private JRadioButton rdbtnRandomNonogram;
+	private JRadioButton rdbtnNonogramBySeed;
+	private JScrollPane scrollPane;
+	private JSlider sliderHeight;
+	private JSlider sliderWidth;
+	private JLabel labelHeight;
+	private JLabel labelWidth;
+	private JButton okButton;
+
+	private boolean validOptions = false;
+	private int type = 0;
+	private Nonogram choosenNono = null;
+	private RandomTypes randomType = null;
+	private String seed = "";
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			NonogramChooserUI dialog = new NonogramChooserUI();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public NonogramChooserUI() {
+		setModal(true);
+		setBounds(100, 100, 308, 475);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+
+		txtNotYetImplemented = new JTextField();
+		txtNotYetImplemented.setText("Not yet implemented");
+		txtNotYetImplemented.setEnabled(false);
+		txtNotYetImplemented.setBounds(68, 387, 221, 19);
+		contentPanel.add(txtNotYetImplemented);
+		txtNotYetImplemented.setColumns(10);
+
+		contentPanel.setLayout(null);
+
+		JLabel lblSeed = new JLabel("Seed:");
+		lblSeed.setBounds(18, 389, 52, 15);
+		contentPanel.add(lblSeed);
+
+		comboBox = new JComboBox();
+		comboBox.setEnabled(false);
+		comboBox.setEditable(false);
+		comboBox.setBounds(68, 266, 221, 24);
+
+		int tmp = RandomTypes.values().length;
+		for (int i = 0; i < tmp; i++) {
+			comboBox.insertItemAt(RandomTypes.values()[i], i);
+		}
+
+		comboBox.setSelectedIndex(0);
+
+		contentPanel.add(comboBox);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+		okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Save chosen options to vars
+				if (rdbtnPredefinedNonograms.isSelected()) {
+					type = 0;
+				} else if (rdbtnRandomNonogram.isSelected()) {
+					type = 1;
+				} else if (rdbtnNonogramBySeed.isSelected()) {
+					type = 2;
+				}
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+				if (node != null) {
+					if (node.getUserObject() instanceof Nonogram) {
+						choosenNono = (Nonogram) node.getUserObject();
+					}
+				}
+				randomType = (RandomTypes) comboBox.getSelectedItem();
+				seed = txtNotYetImplemented.getText();
+
+				if (type == 0 && choosenNono == null) {
+
+				} else {
+					validOptions = true;
+					dispose();
+				}
+
+			}
+		});
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		cancelButton.setActionCommand("Cancel");
+		buttonPane.add(cancelButton);
+
+		rdbtnPredefinedNonograms = new JRadioButton("Predefined Nonograms");
+		rdbtnPredefinedNonograms.setSelected(true);
+		rdbtnPredefinedNonograms.setBounds(8, 8, 229, 23);
+		contentPanel.add(rdbtnPredefinedNonograms);
+		rdbtnPredefinedNonograms.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rdbtnPredefinedNonograms.setSelected(true);
+				tree.setEnabled(true);
+				comboBox.setEnabled(false);
+				txtNotYetImplemented.setEnabled(false);
+				rdbtnNonogramBySeed.setSelected(false);
+				rdbtnRandomNonogram.setSelected(false);
+				sliderWidth.setEnabled(false);
+				sliderHeight.setEnabled(false);
+			}
+		});
+
+		rdbtnRandomNonogram = new JRadioButton("Random Nonogram");
+		rdbtnRandomNonogram.setBounds(8, 235, 229, 23);
+		contentPanel.add(rdbtnRandomNonogram);
+		rdbtnRandomNonogram.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rdbtnRandomNonogram.setSelected(true);
+				tree.setEnabled(false);
+				comboBox.setEnabled(true);
+				txtNotYetImplemented.setEnabled(false);
+				rdbtnPredefinedNonograms.setSelected(false);
+				rdbtnNonogramBySeed.setSelected(false);
+				sliderWidth.setEnabled(true);
+				sliderHeight.setEnabled(true);
+			}
+		});
+
+		rdbtnNonogramBySeed = new JRadioButton("Nonogram by seed");
+		rdbtnNonogramBySeed.setBounds(8, 358, 229, 23);
+		contentPanel.add(rdbtnNonogramBySeed);
+
+		rdbtnNonogramBySeed.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rdbtnNonogramBySeed.setSelected(true);
+				tree.setEnabled(false);
+				comboBox.setEnabled(false);
+				txtNotYetImplemented.setEnabled(true);
+				rdbtnPredefinedNonograms.setSelected(false);
+				rdbtnRandomNonogram.setSelected(false);
+				sliderWidth.setEnabled(false);
+				sliderHeight.setEnabled(false);
+			}
+		});
+
+		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Nonogramme");
+		treeModel = new DefaultTreeModel(rootNode);
+
+		tree = new JTree(treeModel);
+		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		tree.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+					okButton.doClick();
+				}
+			}
+		});
+
+		scrollPane = new JScrollPane();
+		scrollPane.setViewportView(tree);
+		scrollPane.setBounds(68, 39, 221, 188);
+		contentPanel.add(scrollPane);
+
+		sliderHeight = new JSlider();
+		sliderHeight.setSnapToTicks(true);
+		sliderHeight.setValue(5);
+		sliderHeight.setMajorTickSpacing(1);
+		sliderHeight.setMinorTickSpacing(1);
+		sliderHeight.setMinimum(2);
+		sliderHeight.setMaximum(20);
+		sliderHeight.setBounds(138, 302, 112, 16);
+		sliderHeight.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				labelHeight.setText(String.valueOf(sliderHeight.getValue()));
+			}
+		});
+		sliderHeight.setEnabled(false);
+		contentPanel.add(sliderHeight);
+
+		sliderWidth = new JSlider();
+		sliderWidth.setValue(5);
+		sliderWidth.setSnapToTicks(true);
+		sliderWidth.setMinorTickSpacing(1);
+		sliderWidth.setMajorTickSpacing(1);
+		sliderWidth.setMinimum(2);
+		sliderWidth.setMaximum(20);
+		sliderWidth.setBounds(138, 330, 112, 16);
+		sliderWidth.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				labelWidth.setText(String.valueOf(sliderWidth.getValue()));
+			}
+		});
+		sliderWidth.setEnabled(false);
+		contentPanel.add(sliderWidth);
+
+		JLabel lblHhe = new JLabel("Höhe:");
+		lblHhe.setBounds(68, 302, 52, 15);
+		contentPanel.add(lblHhe);
+
+		JLabel lblBreite = new JLabel("Breite:");
+		lblBreite.setBounds(68, 330, 52, 15);
+		contentPanel.add(lblBreite);
+
+		labelHeight = new JLabel("5");
+		labelHeight.setBounds(263, 302, 31, 15);
+		contentPanel.add(labelHeight);
+
+		labelWidth = new JLabel("5");
+		labelWidth.setBounds(263, 330, 31, 15);
+		contentPanel.add(labelWidth);
+
+	}
+
+	public boolean isValidOptions() {
+		return validOptions;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public Nonogram getChoosenNono() {
+		return choosenNono;
+	}
+
+	public RandomTypes getRandomType() {
+		return randomType;
+	}
+
+	public String getSeed() {
+		return seed;
+	}
+
+	public void addNonogramsToTree(String rootNode, Object[] nonograms) {
+		DefaultMutableTreeNode top = new DefaultMutableTreeNode(rootNode);
+		treeModel.insertNodeInto(top, (MutableTreeNode) treeModel.getRoot(), 0);
+
+		for (int i = 0; i < nonograms.length; i++) {
+			treeModel.insertNodeInto(new DefaultMutableTreeNode(nonograms[i]), top, 0);
+		}
+		for (int i = 0; i < tree.getRowCount(); i++) {
+			tree.expandRow(i);
+		}
+	}
+
+	public int getSliderHeight() {
+		return sliderHeight.getValue();
+	}
+
+	public int getSliderWidth() {
+		return sliderWidth.getValue();
+	}
+}
