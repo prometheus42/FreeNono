8b708026b1089cfd85fe82904cd8d84575e99a40
wichmannc <wichmannc@1b927d05-71d8-4c19-9105-a0ed4ac3fa1a>
2012-02-28 18:51:09 +0000

    * Import of mp2fn converter version 0.1 Date: 2011-07-21 20:13:36

---
+++ trunk/mp2fn/src/mp2fn.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Name:         mp2fn.py
+# Purpose:      Mario's Picross to FreeNono Converter
+#
+# Author:       Christian Wichmann
+#
+# Created:      2010-12-19
+# Licence:      GNU GPL
+#----------------------------------------------------------------------------
+
+import os, glob
+import Image
+
+appTitle = 'mp2fn'
+appVersion = '0.1'
+appAuthor = 'Christian Wichmann'
+rootDir = './'
+screenPath = "../ScreenShots/"
+levelPath = "../LevelData/"
+width = 15
+height = 15
+DEBUG = False
+
+
+def convertNonograms():    
+    # read rootDir
+    fileList = glob.glob(rootDir+screenPath+'*.png')
+    
+    # process every png file in rootDir
+    for everyFile in fileList:
+        fileName = os.path.split(everyFile)[1]
+        imageName = fileName.rsplit('.',1)[0]
+        #
+        # read image file
+        try:
+            im = Image.open(everyFile)
+        except IOError:
+            print "cannot open image", fileName
+            return
+        #
+        # checking image size
+        if (im.size != (160,144)):
+            print "wrong image size", fileName
+            return
+        #
+        # processing image
+        nonogram = ""
+        #
+        # image structure:
+        # (0.0)
+        #     * (61,54)  ...   * (146,54)
+        #     ...        ...   ....
+        #     * (61,138) ...   * (146,138)
+        #
+        for y in range(54,140,6):
+            for x in range(61,150,6):
+                # getpixel: returns either (176, 176, 176) for empty box 
+                #           or (80, 80, 80) for full box
+                pixelData = im.getpixel((x,y))
+                if pixelData == (176, 176, 176):
+                    nonogram += " "
+                elif pixelData == (80, 80, 80):
+                    nonogram += "x"
+                else:
+                    print "wrong image format", fileName
+                    return
+            nonogram += "\n"
+        #
+        # writing nonogram to file
+        levelFile = open(rootDir+levelPath+imageName+".nonogram","w")
+        levelFile.write("Mario's Picross "+imageName+"\n"+str(width)+","+str(height)+"\n"+nonogram)
+        levelFile.close()
+        print 'File ' + everyFile + ' converted...'
+
+
+if __name__ == '__main__':
+    #arg = sys.argv
+    #filename = arg[1]
+
+    # print info message
+    print 'This is', appTitle, 'ver.', appVersion, 'by', appAuthor
+    print '(c) 2011 by', appAuthor
+
+    # start converting nonogram files        
+    convertNonograms()    
+    
+    # banner again
+    print 'Have a nice day!'
+
