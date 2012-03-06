da635a5484bd9172436550b02d493bff807fde8d
wichmannc <wichmannc@1b927d05-71d8-4c19-9105-a0ed4ac3fa1a>
2012-03-06 12:35:52 +0000

    + Added interfaces for CollectionProvider, CourseProvider and NonogramProvider + Added nonogram from filesystem implementation for all three providers + Added nonogram from seed implementation for all three providers

---
+++ trunk/FreeNono/src/org/freenono/provider/NonogramFromFilesystem.java
@@ -0,0 +1,95 @@
+/*****************************************************************************
+ * FreeNono - A free implementation of the nonogram game
+ * Copyright (c) 2012 Christian Wichmann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+package org.freenono.provider;
+
+import org.freenono.interfaces.NonogramProvider;
+import org.freenono.model.Course;
+import org.freenono.model.DifficultyLevel;
+import org.freenono.model.Nonogram;
+
+public class NonogramFromFilesystem implements NonogramProvider {
+
+	private Nonogram nonogram = null;
+
+	public NonogramFromFilesystem() {
+
+	}
+
+	public NonogramFromFilesystem(Nonogram n) {
+
+		this();
+
+		this.nonogram = n;
+
+	}
+
+	// TODO: change class so that the nonogram is only loaded from filesystem,
+	// if this function is called!
+	@Override
+	public Nonogram fetchNonogram() {
+
+		return nonogram;
+
+	}
+
+	@Override
+	public String getId() {
+
+		return fetchNonogram().getId();
+	}
+
+	@Override
+	public String getName() {
+		// TODO: change this function to not read nonogram. Instead use filename
+		// directly to get name of nonogram.
+
+		return fetchNonogram().getName();
+
+	}
+
+	@Override
+	public String getDescription() {
+
+		return fetchNonogram().getDescription();
+
+	}
+
+	@Override
+	public DifficultyLevel getDifficulty() {
+
+		return fetchNonogram().getDifficulty();
+
+	}
+	
+	public String toString(){
+		
+		return getName();
+		
+	}
+
+	@Override
+	public int width() {
+		return fetchNonogram().width();
+	}
+
+	@Override
+	public int height() {
+		return fetchNonogram().height();
+	}
+
+}
