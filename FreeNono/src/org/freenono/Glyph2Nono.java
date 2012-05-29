b904368145da6f97df75139650e35a7448063528
Christian Wichmann <prometheus42@users.noreply.github.com>
2012-05-29 11:18:22 +0000

    + Added JLPT 1 Kanji course + Added Kanji Class 1 to 6 courses * Enhanced Glyph2Nono class for creating nonograms from font glyphs

--- FreeNono/src/org/freenono/Glyph2Nono.java
+++ FreeNono/src/org/freenono/Glyph2Nono.java
@@ -23,64 +23,181 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.imageio.ImageIO;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.freenono.model.Course;
 import org.freenono.model.DifficultyLevel;
 import org.freenono.model.Nonogram;
 import org.freenono.serializer.XMLCourseSerializer;
 import org.freenono.serializer.ZipCourseSerializer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 
 public class Glyph2Nono {
 
-	public static List<String> chars = new ArrayList<String>();
-	public static List<BufferedImage> pics = new ArrayList<BufferedImage>();
-	public static Map<String, String> chars2 = new HashMap<String, String>();
+	private List<Kanji> chars = new ArrayList<Kanji>();
+	private List<BufferedImage> pics = new ArrayList<BufferedImage>();
+	private String courseName = null;
 	
-	public static Font font = null;
-	public static int imgWidth = 30, imgHeight = 30;
+	private Font font = null;
+	private int imgWidth = 30, imgHeight = 30;
 
+	public class Kanji {
+		private String name = null;
+		private String description = null;
+		private String kanji = null;
+		
+		public Kanji(String name, String kanji, String description) {
+			this.name = name;
+			this.description = description;
+			this.kanji = kanji;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public String getKanji() {
+			return kanji;
+		}
+		public void setKanji(String kanji) {
+			this.kanji = kanji;
+		}
+	}
 	
 	public static void main(String[] args) {
 
-		font = new Font("MS Mincho", Font.PLAIN, 32);//"MS Gothic"
-		
-		loadChars();
-		
-		//loadCharsFromHTML();
-
-		convertCharToImage();
-		
-		convertImageToNonogram();
+		new Glyph2Nono();
 	}
 
 	
-	public static void loadChars() {
+	public Glyph2Nono() {
+		
+		font = new Font("MS Mincho", Font.PLAIN, 32); //"MS Gothic"
+		
+		courseName = "JLPT 1 Kanji";//"Kanji Class 6"
+	
+		// load kanji information from html files
+		//loadCharsFromHTML("/home/christian/Desktop/Nonogramme/Klasse6Kanji.html");
+		
+		// load kanji into data structure from string
+		loadChars();
+		
+		// convert all saved kanji to nonograms
+		convertCharToImage();
+		convertImageToNonogram();
+	}
+	
+	public void loadChars() {
 
 		String jlpt1 = "日一国人年大十二本中長出三時行見月分後前生五間上東四今金九入学高円子外八六下来気小七山話女北午百書先名川千水半男西電校語土木聞食車何南万毎白天母火右読友左休父雨";
 		
 		for (int i = 0; i < jlpt1.length(); i++) {
 			
-			chars.add(String.valueOf(jlpt1.charAt(i)));
+			String name = String.valueOf(jlpt1.charAt(i));
+			String description = "Kanji " + name;
+			chars.add(new Kanji(name, name, description));
 		}
 	}
 	
-	public static void loadCharsFromHTML() {
+	public void loadCharsFromHTML(String filename) {
 		
+		DocumentBuilder parser;
+		try {
+			parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+			Document doc = parser.parse(new FileInputStream(new File(filename)));
+
+			Element root = doc.getDocumentElement();
+			
+			Element table = (Element) root.getElementsByTagName("table").item(0);
+			
+			if (table != null) {
+				
+				NodeList rowList = table.getElementsByTagName("tr");
+
+				// run through all rows of table
+				for (int i = 0; i < rowList.getLength(); i++) {
+					
+					Node node = rowList.item(i);
+					NodeList columnFromRow = node.getChildNodes();
+					
+					String name = null;
+					String description = null;
+					String kanji = null;
+					
+					// read all children of row and assign data to data structures
+					for (int j = 0; j < columnFromRow.getLength(); j++) {
+					
+						Node node2 = columnFromRow.item(j);
+						
+						switch (j) {
+						case 1:
+							kanji = node2.getTextContent();
+							name = kanji;
+							break;
+						case 3:
+							name = name + " - " + node2.getTextContent();
+							break;
+						case 5:
+							description = "On'yomi: " + node2.getTextContent() + ", ";
+							break;
+						case 7:
+							description = description + "Kun'yomi: " + node2.getTextContent();
+							break;
+						default:
+							break;
+						}
+					}
+					System.out.println(name+" - "+description);
+					chars.add(new Kanji(name, kanji, description));
+				}
+			}
+
+		} catch (ParserConfigurationException e) {
+
+			e.printStackTrace();
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+
+		} catch (SAXException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
 	}
 
-	private static void convertCharToImage() {
+	private void convertCharToImage() {
 
-		for (String cc : chars) {
+		for (Kanji cc : chars) {
 
 			BufferedImage img = new BufferedImage(imgWidth, imgHeight,
 					BufferedImage.TYPE_BYTE_GRAY);
@@ -93,7 +210,7 @@ public class Glyph2Nono {
 			g.setFont(font);
 
 			FontMetrics fm = g.getFontMetrics(font);
-			Rectangle2D rect = fm.getStringBounds(cc, g);
+			Rectangle2D rect = fm.getStringBounds(cc.getKanji(), g);
 
 			int textHeight = (int) (rect.getHeight());
 			int textWidth = (int) (rect.getWidth());
@@ -101,23 +218,13 @@ public class Glyph2Nono {
 			int x = (imgWidth - textWidth) / 2;
 			int y = (imgHeight - textHeight) / 2 + fm.getAscent();
 
-			g.drawString(cc, x, y); // Draw the string.
+			g.drawString(cc.getKanji(), x, y); // Draw the string.
 
-			try {
-
-				ImageIO.write((RenderedImage) img, "png", new File(
-						"/home/christian/Desktop/" + cc + ".png"));
-
-			} catch (IOException e) {
-
-				e.printStackTrace();
-			}
-			
 			pics.add(img);
 		}
 	}
 	
-	private static void convertImageToNonogram() {
+	private void convertImageToNonogram() {
 		
 		List<Nonogram> listNonograms = new ArrayList<Nonogram>();
 		
@@ -134,16 +241,17 @@ public class Glyph2Nono {
 				}
 			}
 			
-			Nonogram n = new Nonogram(chars.get(pics.indexOf(img)), DifficultyLevel.undefined, field);
+			Nonogram n = new Nonogram(chars.get(pics.indexOf(img)).getName(), 
+					DifficultyLevel.undefined, field);
 			n.setAuthor("Christian Wichmann");
 			n.setLevel(pics.indexOf(img));
 			n.setDifficulty(DifficultyLevel.undefined);
-			n.setDescription("Kanji " + chars.get(pics.indexOf(img)));
+			n.setDescription(chars.get(pics.indexOf(img)).getDescription());
 			
 			listNonograms.add(n);
 		}
 
-		Course c = new Course("JLPT 1 Kanji", listNonograms);
+		Course c = new Course(courseName, listNonograms);
 		try {
 			// new XMLCourseSerializer().save(new File(
 			// "/home/christian/.FreeNono/nonograms/kanjitest/"), c);
