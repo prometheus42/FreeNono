6d52ef53b495289ed9f72f3a332dc782a10e0e64
wichmannc <wichmannc@1b927d05-71d8-4c19-9105-a0ed4ac3fa1a>
2013-03-15 19:09:25 +0000

    * Enhanced new NonogramExplorer class

--- trunk/FreeNono/src/org/freenono/ui/NonogramExplorer.java
+++ trunk/FreeNono/src/org/freenono/ui/NonogramExplorer.java
@@ -17,17 +17,27 @@
  *****************************************************************************/
 package org.freenono.ui;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Dialog.ModalityType;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.util.List;
 
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.ListSelectionModel;
 
 import org.apache.log4j.Logger;
 import org.freenono.controller.ColorModel;
@@ -47,6 +57,8 @@ public class NonogramExplorer extends JDialog {
 	
 	private GridBagLayout layout;
 	private JTabbedPane collectionPane;
+	private JPanel maintenancePane;
+	private JPanel collectionMaintenancePane;
 
 	private List<CollectionProvider> nonogramProvider;
 	private Nonogram chosenNonogram = null;
@@ -78,17 +90,17 @@ public class NonogramExplorer extends JDialog {
 				"/resources/icon/icon_freenono.png")).getImage());
 
 		// set layout manager
-		layout = new GridBagLayout();
-		setLayout(layout);
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.gridheight = 1;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.BOTH;
+		// layout = new GridBagLayout();
+		// setLayout(layout);
+		// GridBagConstraints c = new GridBagConstraints();
+		// c.gridx = 0;
+		// c.gridy = 0;
+		// c.gridheight = 1;
+		// c.gridwidth = 1;
+		// c.anchor = GridBagConstraints.CENTER;
+		// c.fill = GridBagConstraints.BOTH;
 		
-		add(getTabbedPane(), c);
+		add(getTabbedPane());
 		pack();
 	}
 
@@ -105,6 +117,11 @@ public class NonogramExplorer extends JDialog {
 			
 		}
 		
+		// add maintenance tab
+		collectionPane.addTab("Maintenance", new ImageIcon(getClass()
+				.getResource("/resources/icon/CollectionMaintenance.png")),
+				getMaintenanceTab(), "Maintenance");
+
 		return collectionPane;
 	}
 
@@ -155,12 +172,106 @@ public class NonogramExplorer extends JDialog {
 		return jp;
 	}
 
+	private JPanel getMaintenanceTab() {
+		
+		maintenancePane = new JPanel();
+
+		// set layout manager and constraints
+		maintenancePane.setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridheight = 4;
+		c.gridwidth = 3;
+		c.anchor = GridBagConstraints.CENTER;
+		c.fill = GridBagConstraints.BOTH;
+		
+		// add listBox
+		String[] data = new String[nonogramProvider.size()];
+		int i = 0;
+		for (CollectionProvider collection : nonogramProvider) {
+			data[i++] = collection.getProviderName();
+		}
+		JList collectionList = new JList();
+		//collectionList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		//collectionList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		JScrollPane listScroller = new JScrollPane(collectionList);
+		listScroller.setPreferredSize(new Dimension(250, 400));
+		maintenancePane.add(listScroller, c);
+		
+		// add buttons
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridheight = 1;
+		c.gridwidth = 1;
+		c.anchor = GridBagConstraints.SOUTHWEST;
+		c.fill = GridBagConstraints.NONE;
+		maintenancePane.add(new JButton("-"),c);
+		
+		c.gridx = 2;
+		c.gridy = 4;
+		c.gridheight = 1;
+		c.gridwidth = 1;
+		c.anchor = GridBagConstraints.SOUTHEAST;
+		c.fill = GridBagConstraints.NONE;
+		maintenancePane.add(new JButton("+"),c);
+		
+		// add selection for new collection
+		c.gridx = 4;
+		c.gridy = 0;
+		c.gridheight = 3;
+		c.gridwidth = 2;
+		c.anchor = GridBagConstraints.CENTER;
+		c.fill = GridBagConstraints.NONE;
+		JRadioButton collectionFilesystemButton = new JRadioButton("Collection from Filesystem");
+		JRadioButton collectionSeedButton = new JRadioButton("Collection from Seed");
+		JRadioButton collectionServerButton = new JRadioButton("Collection from Server");
+		ButtonGroup group = new ButtonGroup();
+		group.add(collectionFilesystemButton);
+		group.add(collectionSeedButton);
+		group.add(collectionServerButton);
+		JPanel radioButtonPane = new JPanel(new GridLayout(0, 1));
+		radioButtonPane.add(collectionFilesystemButton);
+		radioButtonPane.add(collectionSeedButton);
+		radioButtonPane.add(collectionServerButton);
+		maintenancePane.add(radioButtonPane,c);
+		
+		// collection maintenance pane
+		c.gridx = 4;
+		c.gridy = 4;
+		c.gridheight = 2;
+		c.gridwidth = 2;
+		c.anchor = GridBagConstraints.CENTER;
+		c.fill = GridBagConstraints.BOTH;
+		collectionMaintenancePane = new JPanel();
+		maintenancePane.add(collectionMaintenancePane,c);
+		
+		// set listener for radio buttons
+		ActionListener showFilesystemPane = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				collectionMaintenancePane.removeAll();
+				collectionMaintenancePane.add(new JButton("Filesystem"));
+				collectionMaintenancePane.validate();
+			}
+		};
+		ActionListener showServerPane = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				collectionMaintenancePane.removeAll();
+				collectionMaintenancePane.add(new JButton("Server"));
+				collectionMaintenancePane.validate();
+			}
+		};
+		collectionFilesystemButton.addActionListener(showFilesystemPane);
+		//collectionSeedButton.addActionListener(showSeedPane);
+		collectionServerButton.addActionListener(showServerPane);
+		
+		return maintenancePane;
+	}
 	
-	
-	
-	
-	
-	
+
 	public Nonogram getChosenNonogram() {
 
 		return chosenNonogram;
