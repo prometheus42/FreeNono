155bfbe7ac5b5784f018ae7c9bda95c66d93b180
erebos42 <erebos42@1b927d05-71d8-4c19-9105-a0ed4ac3fa1a>
2013-06-09 18:59:08 +0000

    Creating branch for code style refactoring.

---
+++ branches/codestyle/src/org/freenono/ui/AboutDialog.java
@@ -0,0 +1,150 @@
+/*****************************************************************************
+ * FreeNono - A free implementation of the nonogram game
+ * Copyright (c) 2013 by FreeNono Development Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+package org.freenono.ui;
+
+import javax.swing.JPanel;
+
+import java.awt.Frame;
+import java.awt.BorderLayout;
+
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.WindowConstants;
+
+
+/**
+ * Shows a about dialog.
+ * 
+ * @author Christian Wichmann
+ */
+public class AboutDialog extends JDialog {
+
+	private static final long serialVersionUID = -78784201445320344L;
+
+	//private static Logger logger = Logger.getLogger(AboutDialog.class);
+	
+	private JPanel jContentPane = null;
+	
+	private JEditorPane jPane = null;
+	
+	/**
+	 * @param owner
+	 */
+	public AboutDialog(Frame owner) {
+		super(owner);
+		initialize();
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		this.setSize(600, 400);
+		this.setModalityType(ModalityType.APPLICATION_MODAL);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setAlwaysOnTop(true);
+		this.setUndecorated(true);
+		
+		this.setContentPane(getJContentPane());
+	}
+
+	/**
+	 * This method initializes jContentPane
+	 * 
+	 * @return javax.swing.JPanel
+	 */
+	private JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jContentPane = new JPanel();
+			BorderLayout borderLayout = new BorderLayout();
+			jContentPane.setLayout(borderLayout);
+			jContentPane.add(getJPane(), BorderLayout.CENTER);
+		}
+		return jContentPane;
+	}
+
+	private JEditorPane getJPane() {
+		if (jPane == null) {
+			jPane = new JEditorPane();
+			jPane.setEditable(false);
+			
+			/* close About box when mouse is clicked or key is pressed */
+			jPane.addKeyListener(new java.awt.event.KeyAdapter() {
+				
+				@Override
+				public void keyPressed(java.awt.event.KeyEvent e) {
+					close();
+				}
+				
+				@Override
+				public void keyTyped(java.awt.event.KeyEvent e) {
+					close();
+				}
+				
+				@Override
+				public void keyReleased(java.awt.event.KeyEvent e) {
+					close();
+				}
+			});
+			jPane.addMouseListener(new java.awt.event.MouseAdapter() {
+				
+				@Override
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					close();
+				}
+				
+				@Override
+				public void mousePressed(java.awt.event.MouseEvent e) {
+					close();
+				}
+				
+				@Override
+				public void mouseReleased(java.awt.event.MouseEvent e) {
+					close();
+				}
+			});
+					
+			/* insert content for editor pane */
+			String type = "text/html";
+			jPane.setContentType(type);
+			jPane.setEditorKit(jPane.getEditorKitForContentType(type));
+			String content = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">";
+			content += "<html><body style=\"text-orientation:center;background-color:#A68FE7;\"><div style=\"background-color:#E7E08F;";
+			content += "width:460px;height:300px;padding: 10px;margin: 15px;border:3px green solid\">";
+			content += "<h1 style=\"letter-spacing: 0.25em;text-shadow: #AAA 2px 2px 2px;border-bottom: 1px solid black;";
+			content += "padding: 0 0 0.25em 0;\">FreeNono::About</h1><dl style=\"font-family:monospace;color:black;";
+			content += "margin:10em;border:2px solid #8B77C3;\">";
+			content += "<dt>Software Architect</dt><dd>Markus Wichmann</dd>";
+			content += "<dt>Graphical Concept and Software Developer</dt><dd>Christian Wichmann</dd>";
+			content += "<dt>Musical Coordinator</dt><dd>Martin Wichmann</dd>";
+			content += "</dl></div></body></html>";
+			jPane.setText(content);
+		}
+		return jPane;
+	}
+
+	private void close() {
+		setVisible(false);
+		dispose();
+	}
+	
+}
---
+++ branches/codestyle/src/org/freenono/ui/SplashScreen.java
@@ -0,0 +1,169 @@
+/*****************************************************************************
+ * FreeNono - A free implementation of the nonogram game
+ * Copyright (c) 2013 by FreeNono Development Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+package org.freenono.ui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.imageio.ImageIO;
+import javax.swing.JDialog;
+import javax.swing.WindowConstants;
+
+public class SplashScreen extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final int TIMER_DELAY = 3000;
+	private Integer timerDelay = TIMER_DELAY;
+	
+	private final Timer timer = new Timer();
+	
+	private static final String DEFAULT_RESSOURCE = "/resources/icon/splashscreen.png";
+	private String ressource = DEFAULT_RESSOURCE;
+	
+	private BufferedImage image = null;
+	
+
+	public SplashScreen(int timerDelay) {
+		
+		super();
+		
+		this.setTimerDelay(timerDelay);
+		
+		loadImage();
+		
+		initialize();
+		
+		if (timerDelay != 0)
+			setupTimer();
+	}
+
+	public SplashScreen(String ressource) {
+		
+		super();
+		
+		this.ressource = ressource;
+		
+		loadImage();
+		
+		initialize();
+		
+		setupTimer();
+	}
+	
+	private void loadImage() {
+		
+		try {
+			image = ImageIO.read(getClass().getResource(ressource));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	private void setupTimer() {
+		
+		timer.schedule(new TimerTask() {
+			public void run() {
+				close();
+			}
+		}, TIMER_DELAY);
+	}
+
+	public Integer getTimerDelay() {
+		
+		return timerDelay;
+	}
+
+	public void setTimerDelay(Integer timerDelay) {
+		
+		this.timerDelay = timerDelay;
+	}
+
+	
+	private void initialize() {
+		
+		this.setSize(image.getWidth(), image.getHeight());
+		this.setResizable(false);
+		this.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+		this.setModalityType(ModalityType.APPLICATION_MODAL);
+		this.setLocationRelativeTo(null);
+		this.setAlwaysOnTop(true);
+		this.setTitle("");
+		this.addKeyListener(new java.awt.event.KeyAdapter() {
+			
+			@Override
+			public void keyPressed(java.awt.event.KeyEvent e) {
+				close();
+			}
+			
+			@Override
+			public void keyTyped(java.awt.event.KeyEvent e) {
+				close();
+			}
+			
+			@Override
+			public void keyReleased(java.awt.event.KeyEvent e) {
+				close();
+			}
+		});
+		this.addMouseListener(new java.awt.event.MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(java.awt.event.MouseEvent e) {
+				close();
+			}
+			
+			@Override
+			public void mousePressed(java.awt.event.MouseEvent e) {
+				close();
+			}
+			
+			@Override
+			public void mouseReleased(java.awt.event.MouseEvent e) {
+				close();
+			}
+		});
+		this.setAlwaysOnTop(true);
+		this.setUndecorated(true);
+	}
+
+	private void close() {
+		
+		setVisible(false);
+		dispose();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		
+		super.paintComponents(g);
+
+		g.drawImage(
+				image,
+				0,
+				0,
+				this.getWidth(),
+				this.getHeight(),
+				this);
+	}
+
+}
