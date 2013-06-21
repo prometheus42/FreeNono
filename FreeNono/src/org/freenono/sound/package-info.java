0a1feafae3400fd558a176aa60f84f450b1c8e3c
Christian Wichmann <prometheus42@users.noreply.github.com>
2013-06-21 10:42:59 +0000

    + Added package info file.

---
+++ FreeNono/src/org/freenono/sound/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides the audio provider which controls all audio output of
+ * FreeNono. It instantiates AudioPlayer classes for all background
+ * music and sound effects and reacts on events fired by the game.
+ * 
+ * @author Christian Wichmann
+ *
+ */
+package org.freenono.sound;
