4435ad3e314df00d54b16cc078c4583874ec3f7e
Christian Wichmann <prometheus42@users.noreply.github.com>
2013-06-23 09:53:50 +0000

    * Added options playMusic and playEffects to xml settings serializer.

---
+++ FreeNono/src/org/freenono/serializer/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides serializer to store settings, highscores and seed data as
+ * files, eg. as xml files.
+ * 
+ * @author Markus Wichmann, Christian Wichmann
+ */
+package org.freenono.serializer;
