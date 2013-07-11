6b8b37bb0dc53b62412855f978b09f4c8dce15c8
Christian Wichmann <prometheus42@users.noreply.github.com>
2013-07-11 15:45:20 +0000

    + Created package org.freenono.model.data.

--- lib_data/src/org/freenono/model/data/package-info.java
+++ lib_data/src/org/freenono/model/data/package-info.java
@@ -1,27 +1,26 @@
 /*****************************************************************************
  * FreeNono - A free implementation of the nonogram game
  * Copyright (c) 2013 by FreeNono Development Team
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 
 /**
- * Provides all classes that represent the game model. Included are
- * data holding classes like GameBoard and Seed, enums like Token,
- * GameState and GameModeType, all implementations for the game modes.
+ * Provides all necessary data classes like nonograms, courses etc. that will
+ * be used by FreeNono and all associated projects.
  * 
- * @author Christian Wichmann, Markus Wichmann
+ * @author Markus Wichmann, Christian Wichmann
  */
-package org.freenono.model;
+package org.freenono.model.data;
 
