f0fd06550db7d07d2ac476422d9c395c1af22513
wichmannc <wichmannc@1b927d05-71d8-4c19-9105-a0ed4ac3fa1a>
2013-07-11 14:34:30 +0000

    * Deleted one of two package info files for model and serializer packages.

--- trunk/FreeNono/src/org/freenono/serializer/package-info.java
+++ trunk/FreeNono/src/org/freenono/serializer/package-info.java
@@ -1,7 +1,31 @@
+/*****************************************************************************
+ * FreeNono - A free implementation of the nonogram game
+ * Copyright (c) 2013 by FreeNono Development Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+
 /**
- * Provides serializer to store settings, highscores and seed data as
- * files, eg. as xml files.
+ * Provides all serializer to store data (settings, highscores and seed data)
+ * in files or transmit them over network. Included are:  
+ *  - nonogram serializer,
+ *  - course serializer,
+ *  - settings serializer (only for the FreeNono project),
+ *  - seed serializer (only for CollectionFromSeed nonogram provider),
+ *  - highscore serializer.
  * 
  * @author Markus Wichmann, Christian Wichmann
  */
 package org.freenono.serializer;
+
