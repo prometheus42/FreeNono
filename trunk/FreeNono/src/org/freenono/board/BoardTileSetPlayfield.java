cb067579320fc47994d7c9f42b0df4348d424c1a
wichmannc <wichmannc@1b927d05-71d8-4c19-9105-a0ed4ac3fa1a>
2013-08-15 11:12:43 +0000

    * Cleaned up imports.

--- trunk/FreeNono/src/org/freenono/board/BoardTileSetPlayfield.java
+++ trunk/FreeNono/src/org/freenono/board/BoardTileSetPlayfield.java
@@ -20,14 +20,11 @@ package org.freenono.board;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.swing.AbstractAction;
-import javax.swing.InputMap;
 import javax.swing.JComponent;
 import javax.swing.KeyStroke;
 import javax.swing.Scrollable;
