e60cbcdb7728d0a36aea406621136caa97975666
wichmannc <wichmannc@1b927d05-71d8-4c19-9105-a0ed4ac3fa1a>
2014-04-14 08:23:49 +0000

    * Added more information to tool tip on course view tree.

--- trunk/FreeNono/src/org/freenono/ui/explorer/NonogramTreeRenderer.java
+++ trunk/FreeNono/src/org/freenono/ui/explorer/NonogramTreeRenderer.java
@@ -18,12 +18,18 @@
 package org.freenono.ui.explorer;
 
 import java.awt.Component;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
+import org.freenono.controller.HighscoreManager;
+import org.freenono.controller.Score;
 import org.freenono.controller.SimpleStatistics;
 import org.freenono.provider.CourseProvider;
 import org.freenono.provider.NonogramProvider;
@@ -37,7 +43,10 @@ import org.freenono.ui.Messages;
 public class NonogramTreeRenderer extends DefaultTreeCellRenderer {
 
     private static final long serialVersionUID = -1903332761908135884L;
-    private ImageIcon icon;
+
+    private static final DateFormat DATE_FORMAT = SimpleDateFormat
+            .getDateInstance(SimpleDateFormat.SHORT);
+    private final ImageIcon icon;
 
     /**
      * Initializes an own tree cell renderer for the nonogram tree in
@@ -56,27 +65,50 @@ public class NonogramTreeRenderer extends DefaultTreeCellRenderer {
             final Object value, final boolean sel, final boolean expanded,
             final boolean leaf, final int row, final boolean hasFocus) {
 
+        /*
+         * For all leafs in the tree that stand for a nonogram course a tool tip
+         * is added with statistical information about this course.
+         */
+
         super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf,
                 row, hasFocus);
 
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
         if (node.getUserObject() instanceof CourseProvider) {
 
-            int unsolvedNonograms = countUnsolvedNonograms((CourseProvider) (node
-                    .getUserObject()));
+            final Object userObject = node.getUserObject();
 
             if (leaf) {
+                final StringBuilder tooltipText = new StringBuilder("<html>");
+                final int unsolvedNonograms = countUnsolvedNonograms((CourseProvider) userObject);
+
+                // construct tool tip text from number of solved nonograms...
                 if (unsolvedNonograms == 0) {
                     setIcon(icon);
-                    setToolTipText(Messages
-                            .getString("NonogramChooserUI.NonogramTreeCompleteCourse"));
+                    tooltipText
+                            .append(Messages
+                                    .getString("NonogramChooserUI.NonogramTreeCompleteCourse"));
                 } else {
-                    setToolTipText(String.valueOf(unsolvedNonograms)
-                            + " "
-                            + Messages
+                    tooltipText.append(String.valueOf(unsolvedNonograms));
+                    tooltipText.append(" ");
+                    tooltipText
+                            .append(Messages
                                     .getString("NonogramChooserUI.NonogramTreeUnsolvedNonograms"));
                 }
+
+                // ...and date when course was last played.
+                final long dateLastPlayed = determineDateWhenLastPlayed((CourseProvider) userObject);
+                if (dateLastPlayed != 0) {
+                    tooltipText.append("<br>");
+                    tooltipText.append("Last played:");
+                    tooltipText.append(" ");
+                    tooltipText.append(DATE_FORMAT.format(new Date(dateLastPlayed)));
+                }
+                tooltipText.append("</html>");
+                setToolTipText(tooltipText.toString());
+
             } else {
+                // turn off tool tip
                 setToolTipText(null);
             }
         }
@@ -104,5 +136,39 @@ public class NonogramTreeRenderer extends DefaultTreeCellRenderer {
         }
 
         return unsolvedNonogramsInCourse;
+    }
+
+    /**
+     * Determines when the last nonogram from a given course was played. Playing
+     * dates are stored in HighscoreManager and retrieved by nonogram hash.
+     * 
+     * @param cp
+     *            course provider from nonogram tree to be checked
+     * @return time when last nonogram from course was played or 0 when no
+     *         nonogram was ever played.
+     */
+    private long determineDateWhenLastPlayed(final CourseProvider cp) {
+
+        long dateWhenLastPlayed = 0;
+        HighscoreManager hm = HighscoreManager.getInstance();
+
+        for (NonogramProvider np : cp.getNonogramProvider()) {
+            /*
+             * Fetching all highscores for every nonogram in course and check
+             * when the last one was played. This time is returned by the
+             * method. Algorithm based on the assumption that scores are
+             * returned by HighscoreManager sorted by time!
+             */
+            String hash = np.fetchNonogram().getHash();
+            List<Score> list = hm.getHighscoreListForNonogram(hash);
+            if (!list.isEmpty()) {
+                final long currentScore = list.get(0).getTime();
+                if (currentScore > dateWhenLastPlayed) {
+                    dateWhenLastPlayed = currentScore;
+                }
+            }
+        }
+
+        return dateWhenLastPlayed;
     }
 }
