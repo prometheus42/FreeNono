41fbf2679625a61b23a28c8fd9925cd95970a279
Christian Wichmann <prometheus42@users.noreply.github.com>
2015-05-14 14:51:41 +0000

    * Cleaned up Checkstyle warnings for package "org.freenono.serializer".

--- FreeNono/src/org/freenono/serializer/HighscoreFormatException.java
+++ FreeNono/src/org/freenono/serializer/HighscoreFormatException.java
@@ -1,30 +1,29 @@
 /*****************************************************************************
  * FreeNono - A free implementation of the nonogram game
  * Copyright (c) 2013 by FreeNono Development Team
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package org.freenono.serializer;
 
 import javax.activation.UnsupportedDataTypeException;
 
 /**
- * Signals that an error occurred during loading or saving of highscore data
- * to/from file. This exception is the only one given up to the caller, all
- * internal exceptions like file not found or io exception are caught
- * internally.
- * 
+ * Signals that an error occurred during loading or saving of highscore data to/from file. This
+ * exception is the only one given up to the caller, all internal exceptions like file not found or
+ * io exception are caught internally.
+ *
  * @author Christian Wichmann
  */
 public class HighscoreFormatException extends UnsupportedDataTypeException {
@@ -37,7 +36,7 @@ public class HighscoreFormatException extends UnsupportedDataTypeException {
 
     /**
      * Constructs a new <code>HighscoreFormatException</code> with a message.
-     * 
+     *
      * @param message
      *            error message
      */
@@ -46,8 +45,7 @@ public class HighscoreFormatException extends UnsupportedDataTypeException {
     }
 
     /**
-     * Constructs a new <code>HighscoreFormatException</code> with no detail
-     * message.
+     * Constructs a new <code>HighscoreFormatException</code> with no detail message.
      */
     public HighscoreFormatException() {
         super();
