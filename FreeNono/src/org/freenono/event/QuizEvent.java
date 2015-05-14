97b2fc78b44aa259eb586e4a20cc6f04186bbaf0
Christian Wichmann <prometheus42@users.noreply.github.com>
2015-05-14 14:59:52 +0000

    * Cleaned up Checkstyle warnings for package "org.freenono.event".

--- FreeNono/src/org/freenono/event/QuizEvent.java
+++ FreeNono/src/org/freenono/event/QuizEvent.java
@@ -1,28 +1,27 @@
 /*****************************************************************************
  * FreeNono - A free implementation of the nonogram game
  * Copyright (c) 2013 by FreeNono Development Team
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package org.freenono.event;
 
 import org.freenono.quiz.Question;
 
 /**
- * Event used by quiz game mode to exchange questions between game model and
- * user interface.
- * 
+ * Event used by quiz game mode to exchange questions between game model and user interface.
+ *
  * @author Christian Wichmann
  */
 public final class QuizEvent extends GameEvent {
@@ -33,7 +32,7 @@ public final class QuizEvent extends GameEvent {
 
     /**
      * Initializes a quiz event.
-     * 
+     *
      * @param source
      *            Source where event was fired.
      * @param question
@@ -48,7 +47,7 @@ public final class QuizEvent extends GameEvent {
 
     /**
      * Gets the question for this event.
-     * 
+     *
      * @return Question for this event.
      */
     public Question getQuestion() {
@@ -58,7 +57,7 @@ public final class QuizEvent extends GameEvent {
 
     /**
      * Sets question for this event.
-     * 
+     *
      * @param question
      *            Question for this event.
      */
